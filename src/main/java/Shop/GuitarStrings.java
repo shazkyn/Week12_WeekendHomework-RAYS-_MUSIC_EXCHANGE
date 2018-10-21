1bedd27668af9005f1f14e3b95f5b9f97c2e963c
Sharon Kynaston <desmogal@gmail.com>
2018-10-21 17:56:35 +0100

    Guitar Stringss & Picks  created

--- src/main/java/Shop/GuitarStrings.java
+++ src/main/java/Shop/GuitarStrings.java
@@ -1,4 +1,17 @@
 package Shop;
 
-public class GuitarStrings {
+import Behaviour.ISell;
+
+public class GuitarStrings extends Accessories implements ISell {
+
+    public GuitarStrings(String item, double buyPrice, double sellPrice){
+        this.item = item;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    @Override
+    public double markUp() {
+        return 0.5;
+    }
 }
--- src/main/java/Shop/Picks.java
+++ src/main/java/Shop/Picks.java
@@ -1,4 +1,17 @@
 package Shop;
 
-public class Picks {
+import Behaviour.ISell;
+
+public class Picks extends Accessories implements ISell {
+
+    public Picks(String item, double buyPrice, double sellPrice){
+        this.item = item;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    @Override
+    public double markUp() {
+        return 1.0;
+    }
 }
