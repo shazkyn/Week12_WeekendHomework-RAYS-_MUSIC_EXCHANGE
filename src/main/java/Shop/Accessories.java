b79d2dd07614f4bcae63bd7ff8fd974dcf5788ef
Sharon Kynaston <desmogal@gmail.com>
2018-10-21 17:55:24 +0100

    Accesssories & Drumsticks files created

--- src/main/java/Shop/Accessories.java
+++ src/main/java/Shop/Accessories.java
@@ -1,4 +1,21 @@
 package Shop;
 
-public class Accessories {
+import Behaviour.ISell;
+
+public abstract class Accessories implements ISell {
+
+    String item;
+    double buyPrice;
+    double sellPrice;
+
+    public String getItem() {
+        return item;
+    }
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
 }
--- src/main/java/Shop/DrumSticks.java
+++ src/main/java/Shop/DrumSticks.java
@@ -1,4 +1,18 @@
 package Shop;
 
-public class DrumSticks {
+import Behaviour.ISell;
+
+public class DrumSticks extends Accessories implements ISell {
+
+    public DrumSticks(String item, double buyPrice, double sellPrice) {
+
+        this.item = item;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    @Override
+    public double markUp() {
+        return 10;
+    }
 }
