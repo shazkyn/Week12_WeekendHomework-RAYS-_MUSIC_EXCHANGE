f33d715dd57b441dad8d43d92d951653561817d4
Sharon Kynaston <desmogal@gmail.com>
2018-10-21 18:02:05 +0100

    Shop package tested

--- src/main/java/Shop/Shop.java
+++ src/main/java/Shop/Shop.java
@@ -1,4 +1,27 @@
 package Shop;
 
+import Behaviour.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    ArrayList<ISell> stock;
+
+    public Shop(){
+        this.stock = new ArrayList<>();
+    }
+
+    public int stockCount(){
+        return stock.size();
+    }
+
+    public void addStock(ISell item){
+        stock.add(item);
+    }
+
+    public void removeStock(ISell item){
+        stock.remove(item);
+    }
+
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,41 @@
+import Shop.DrumSticks;
+import Shop.GuitarStrings;
+import Shop.Shop;
+import Shop.Picks;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    DrumSticks drumSticks;
+    GuitarStrings guitarStrings;
+    Picks picks;
+    Shop shop;
+
+
+    @Before
+    public void before(){
+        drumSticks = new DrumSticks("Drum sticks", 5.0, 10.0);
+        guitarStrings = new GuitarStrings("Guitar Strings", 10.0, 20.0);
+        picks   = new Picks ("Picks", 5.0,  10.0);
+        shop = new Shop();
+    }
+
+    @Test
+    public void canAddStock(){
+        shop.addStock(picks);
+        assertEquals(shop.stockCount(), 1);
+    }
+
+    @Test
+    public void canremoveStock(){
+        shop.removeStock(picks);
+        assertEquals(shop.stockCount(),0);
+    }
+
+
 }
+
